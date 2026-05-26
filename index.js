f94a1921f01d3586ceda4aa36942aa11bce72da7
Egor Rogozin <99vanders@gmail.com>
2026-05-26 20:42:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // env
 const db = mysql.createPool({
